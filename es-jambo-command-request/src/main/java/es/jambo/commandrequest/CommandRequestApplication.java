777867a5a3dfc31936f65b3dac2f3b3f06ab73ef
Juan Manuel Sánchez Martagón <jmsanchezmartagon@gmail.com>
2023-11-05 13:56:01 +0100

    new. project structure

---
+++ es-jambo-command-request/src/main/java/es/jambo/commandrequest/CommandRequestApplication.java
@@ -0,0 +1,13 @@
+package es.jambo.commandrequest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommandRequestApplication{
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommandRequestApplication.class, args);
+	}
+
+}
---
+++ es-jambo-command-response/src/main/java/es/jambo/commandresponse/CommandResponseApplication.java
@@ -0,0 +1,13 @@
+package es.jambo.commandresponse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommandResponseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommandResponseApplication.class, args);
+	}
+
+}
